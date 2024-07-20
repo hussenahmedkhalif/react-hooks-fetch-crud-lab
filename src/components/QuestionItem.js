8b1f07cd378cc03ad549d18b672b31a99baeaff3
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-07-20 11:49:38 +0300

    updated fetch-crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,17 +9,31 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleSelectChange = (event) => {
+    const newCorrectIndex = parseInt(event.target.value, 10);
+    onUpdate(id, newCorrectIndex);
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIndex} onChange={handleSelectChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => onDelete(id)}>Delete Question</button>
     </li>
   );
 }
 
 export default QuestionItem;
+
+
+
+
+
+
+
